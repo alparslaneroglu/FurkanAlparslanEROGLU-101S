61c49f053fe9bfb130822e4e23f5ca1a0efacd50
furkan <furkan@LAPTOP-NB37UT04>
2021-11-04 21:49:32 +0300

    Add project files.

---
+++ FurkanAlparslanEROGLU-101S/Form2.Designer.cs
@@ -0,0 +1,397 @@
+﻿
+namespace FurkanAlparslanEROGLU_101S
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbSag = new System.Windows.Forms.GroupBox();
+            this.pnl8 = new System.Windows.Forms.Panel();
+            this.lbl8 = new System.Windows.Forms.Label();
+            this.pnl7 = new System.Windows.Forms.Panel();
+            this.lbl7 = new System.Windows.Forms.Label();
+            this.pnl6 = new System.Windows.Forms.Panel();
+            this.lbl6 = new System.Windows.Forms.Label();
+            this.pnl5 = new System.Windows.Forms.Panel();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.pnl4 = new System.Windows.Forms.Panel();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.pnl3 = new System.Windows.Forms.Panel();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.pnl2 = new System.Windows.Forms.Panel();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.pnlust = new System.Windows.Forms.Panel();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.gbSag.SuspendLayout();
+            this.pnl8.SuspendLayout();
+            this.pnl7.SuspendLayout();
+            this.pnl6.SuspendLayout();
+            this.pnl5.SuspendLayout();
+            this.pnl4.SuspendLayout();
+            this.pnl3.SuspendLayout();
+            this.pnl2.SuspendLayout();
+            this.pnlust.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbSag
+            // 
+            this.gbSag.BackColor = System.Drawing.Color.MidnightBlue;
+            this.gbSag.Controls.Add(this.pnl8);
+            this.gbSag.Controls.Add(this.pnl7);
+            this.gbSag.Controls.Add(this.pnl6);
+            this.gbSag.Controls.Add(this.pnl5);
+            this.gbSag.Controls.Add(this.pnl4);
+            this.gbSag.Controls.Add(this.pnl3);
+            this.gbSag.Controls.Add(this.pnl2);
+            this.gbSag.Dock = System.Windows.Forms.DockStyle.Right;
+            this.gbSag.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.gbSag.ForeColor = System.Drawing.Color.Gold;
+            this.gbSag.Location = new System.Drawing.Point(1101, 0);
+            this.gbSag.Name = "gbSag";
+            this.gbSag.Size = new System.Drawing.Size(176, 450);
+            this.gbSag.TabIndex = 5;
+            this.gbSag.TabStop = false;
+            this.gbSag.Text = "***";
+            // 
+            // pnl8
+            // 
+            this.pnl8.BackColor = System.Drawing.Color.RoyalBlue;
+            this.pnl8.Controls.Add(this.lbl8);
+            this.pnl8.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnl8.Location = new System.Drawing.Point(1023, 23);
+            this.pnl8.Name = "pnl8";
+            this.pnl8.Size = new System.Drawing.Size(170, 424);
+            this.pnl8.TabIndex = 0;
+            this.pnl8.Visible = false;
+            // 
+            // lbl8
+            // 
+            this.lbl8.BackColor = System.Drawing.Color.Yellow;
+            this.lbl8.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lbl8.Location = new System.Drawing.Point(0, 0);
+            this.lbl8.Name = "lbl8";
+            this.lbl8.Size = new System.Drawing.Size(170, 23);
+            this.lbl8.TabIndex = 0;
+            this.lbl8.Text = "7";
+            this.lbl8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pnl7
+            // 
+            this.pnl7.BackColor = System.Drawing.Color.RoyalBlue;
+            this.pnl7.Controls.Add(this.lbl7);
+            this.pnl7.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnl7.Location = new System.Drawing.Point(853, 23);
+            this.pnl7.Name = "pnl7";
+            this.pnl7.Size = new System.Drawing.Size(170, 424);
+            this.pnl7.TabIndex = 6;
+            this.pnl7.Visible = false;
+            // 
+            // lbl7
+            // 
+            this.lbl7.BackColor = System.Drawing.Color.Yellow;
+            this.lbl7.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lbl7.Location = new System.Drawing.Point(0, 0);
+            this.lbl7.Name = "lbl7";
+            this.lbl7.Size = new System.Drawing.Size(170, 23);
+            this.lbl7.TabIndex = 0;
+            this.lbl7.Text = "0";
+            this.lbl7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pnl6
+            // 
+            this.pnl6.BackColor = System.Drawing.Color.RoyalBlue;
+            this.pnl6.Controls.Add(this.lbl6);
+            this.pnl6.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnl6.Location = new System.Drawing.Point(683, 23);
+            this.pnl6.Name = "pnl6";
+            this.pnl6.Size = new System.Drawing.Size(170, 424);
+            this.pnl6.TabIndex = 5;
+            this.pnl6.Visible = false;
+            // 
+            // lbl6
+            // 
+            this.lbl6.BackColor = System.Drawing.Color.Yellow;
+            this.lbl6.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lbl6.Location = new System.Drawing.Point(0, 0);
+            this.lbl6.Name = "lbl6";
+            this.lbl6.Size = new System.Drawing.Size(170, 23);
+            this.lbl6.TabIndex = 0;
+            this.lbl6.Text = "9";
+            this.lbl6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pnl5
+            // 
+            this.pnl5.BackColor = System.Drawing.Color.RoyalBlue;
+            this.pnl5.Controls.Add(this.lbl5);
+            this.pnl5.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnl5.Location = new System.Drawing.Point(513, 23);
+            this.pnl5.Name = "pnl5";
+            this.pnl5.Size = new System.Drawing.Size(170, 424);
+            this.pnl5.TabIndex = 4;
+            this.pnl5.Visible = false;
+            // 
+            // lbl5
+            // 
+            this.lbl5.BackColor = System.Drawing.Color.Yellow;
+            this.lbl5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lbl5.Location = new System.Drawing.Point(0, 0);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(170, 23);
+            this.lbl5.TabIndex = 0;
+            this.lbl5.Text = "1";
+            this.lbl5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pnl4
+            // 
+            this.pnl4.BackColor = System.Drawing.Color.RoyalBlue;
+            this.pnl4.Controls.Add(this.lbl4);
+            this.pnl4.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnl4.Location = new System.Drawing.Point(343, 23);
+            this.pnl4.Name = "pnl4";
+            this.pnl4.Size = new System.Drawing.Size(170, 424);
+            this.pnl4.TabIndex = 3;
+            this.pnl4.Visible = false;
+            // 
+            // lbl4
+            // 
+            this.lbl4.BackColor = System.Drawing.Color.Yellow;
+            this.lbl4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lbl4.Location = new System.Drawing.Point(0, 0);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(170, 23);
+            this.lbl4.TabIndex = 0;
+            this.lbl4.Text = "B";
+            this.lbl4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pnl3
+            // 
+            this.pnl3.BackColor = System.Drawing.Color.RoyalBlue;
+            this.pnl3.Controls.Add(this.lbl3);
+            this.pnl3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnl3.Location = new System.Drawing.Point(173, 23);
+            this.pnl3.Name = "pnl3";
+            this.pnl3.Size = new System.Drawing.Size(170, 424);
+            this.pnl3.TabIndex = 2;
+            this.pnl3.Visible = false;
+            // 
+            // lbl3
+            // 
+            this.lbl3.BackColor = System.Drawing.Color.Yellow;
+            this.lbl3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lbl3.Location = new System.Drawing.Point(0, 0);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(170, 23);
+            this.lbl3.TabIndex = 0;
+            this.lbl3.Text = "F";
+            this.lbl3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pnl2
+            // 
+            this.pnl2.BackColor = System.Drawing.Color.RoyalBlue;
+            this.pnl2.Controls.Add(this.lbl2);
+            this.pnl2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnl2.Location = new System.Drawing.Point(3, 23);
+            this.pnl2.Name = "pnl2";
+            this.pnl2.Size = new System.Drawing.Size(170, 424);
+            this.pnl2.TabIndex = 1;
+            this.pnl2.Visible = false;
+            // 
+            // lbl2
+            // 
+            this.lbl2.BackColor = System.Drawing.Color.Yellow;
+            this.lbl2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lbl2.Location = new System.Drawing.Point(0, 0);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(170, 23);
+            this.lbl2.TabIndex = 0;
+            this.lbl2.Text = "G";
+            this.lbl2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pnlust
+            // 
+            this.pnlust.BackColor = System.Drawing.Color.MidnightBlue;
+            this.pnlust.Controls.Add(this.button8);
+            this.pnlust.Controls.Add(this.button7);
+            this.pnlust.Controls.Add(this.button6);
+            this.pnlust.Controls.Add(this.button5);
+            this.pnlust.Controls.Add(this.button4);
+            this.pnlust.Controls.Add(this.button3);
+            this.pnlust.Controls.Add(this.button2);
+            this.pnlust.Controls.Add(this.button1);
+            this.pnlust.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlust.Location = new System.Drawing.Point(0, 450);
+            this.pnlust.Name = "pnlust";
+            this.pnlust.Size = new System.Drawing.Size(1277, 70);
+            this.pnlust.TabIndex = 4;
+            // 
+            // button8
+            // 
+            this.button8.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button8.BackColor = System.Drawing.Color.Yellow;
+            this.button8.Location = new System.Drawing.Point(1131, 10);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(134, 48);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "KADIKÖYDEN \'\'ÇIKIŞ\'\' YOK";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click_1);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Yellow;
+            this.button7.Location = new System.Drawing.Point(751, 10);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(117, 48);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "FENERBAHÇE";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Yellow;
+            this.button6.Location = new System.Drawing.Point(628, 10);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(117, 48);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "SÖZÜMÜZ";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Yellow;
+            this.button5.Location = new System.Drawing.Point(505, 10);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(117, 48);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "SON";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Yellow;
+            this.button4.Location = new System.Drawing.Point(382, 10);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(117, 48);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "BİLE";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Yellow;
+            this.button3.Location = new System.Drawing.Point(259, 10);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(117, 48);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "OLSAK";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Yellow;
+            this.button2.Location = new System.Drawing.Point(130, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 48);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "AĞACINDA";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Yellow;
+            this.button1.Location = new System.Drawing.Point(7, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "DAR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Yellow;
+            this.ClientSize = new System.Drawing.Size(1277, 520);
+            this.Controls.Add(this.gbSag);
+            this.Controls.Add(this.pnlust);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.gbSag.ResumeLayout(false);
+            this.pnl8.ResumeLayout(false);
+            this.pnl7.ResumeLayout(false);
+            this.pnl6.ResumeLayout(false);
+            this.pnl5.ResumeLayout(false);
+            this.pnl4.ResumeLayout(false);
+            this.pnl3.ResumeLayout(false);
+            this.pnl2.ResumeLayout(false);
+            this.pnlust.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox gbSag;
+        private System.Windows.Forms.Panel pnl8;
+        private System.Windows.Forms.Label lbl8;
+        private System.Windows.Forms.Panel pnl7;
+        private System.Windows.Forms.Label lbl7;
+        private System.Windows.Forms.Panel pnl6;
+        private System.Windows.Forms.Label lbl6;
+        private System.Windows.Forms.Panel pnl5;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.Panel pnl4;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Panel pnl3;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Panel pnl2;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Panel pnlust;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ FurkanAlparslanEROGLU-101S/Form2.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FurkanAlparslanEROGLU_101S
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        #region PanelIslemleri
+        void Paneller()
+        {
+            gbSag.Visible = true;
+            pnl2.Visible = false;
+            pnl3.Visible = false;
+            pnl4.Visible = false;
+            pnl5.Visible = false;
+            pnl6.Visible = false;
+            pnl7.Visible = false;
+            pnl8.Visible = false;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gbSag.Text = button1.Text;
+            Paneller();
+            pnl2.Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            gbSag.Text = button2.Text;
+            Paneller();
+            pnl3.Visible = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            gbSag.Text = button3.Text;
+            Paneller();
+            pnl4.Visible = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            gbSag.Text = button4.Text;
+            Paneller();
+            pnl5.Visible = true;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            gbSag.Text = button5.Text;
+            Paneller();
+            pnl6.Visible = true;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            gbSag.Text = button6.Text;
+            Paneller();
+            pnl7.Visible = true;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            gbSag.Text = button7.Text;
+            Paneller();
+            pnl8.Visible = true;
+        }
+
+
+        private void button8_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        } 
+        #endregion
+    }
+}
